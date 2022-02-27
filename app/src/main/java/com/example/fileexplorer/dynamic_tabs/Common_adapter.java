fa4ba77039dec2f277c48cfea00ab582c1d95b24
Subhodeep <subhodeeppal64@gmail.com>
2022-02-27 11:55:19 +0530

    27-02-2022 11:55

--- app/src/main/java/com/example/fileexplorer/dynamic_tabs/Common_adapter.java
+++ app/src/main/java/com/example/fileexplorer/dynamic_tabs/Common_adapter.java
@@ -1,19 +1,31 @@
 package com.example.fileexplorer.dynamic_tabs;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
+import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.PopupMenu;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.fileexplorer.R;
+import com.example.fileexplorer.search_view.Detailed_view;
+import com.example.fileexplorer.util.DataHolder;
 
 import java.io.File;
+import java.text.Format;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Common_adapter extends RecyclerView.Adapter<Common_adapter.Common_adapter_ViewHolder> {
     Context context;
@@ -31,10 +43,94 @@ public class Common_adapter extends RecyclerView.Adapter<Common_adapter.Common_a
         return new Common_adapter_ViewHolder(view);
     }
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public void onBindViewHolder(@NonNull Common_adapter_ViewHolder holder, int position) {
         File selectedFile = fileArrayList.get(position);
         holder.fileName.setText(selectedFile.getName());
+        Date lastModified = new Date(selectedFile.lastModified());
+        @SuppressLint("SimpleDateFormat")
+        Format f = new SimpleDateFormat("dd MMM");
+        String date = f.format(lastModified);
+        holder.timeStamp.setText(date);
+
+        if (selectedFile.isDirectory()) {
+            holder.fileIcon.setVisibility(View.GONE);
+            holder.folderIcon.setVisibility(View.VISIBLE);
+        } else if (selectedFile.isFile()) {
+            holder.fileIcon.setVisibility(View.VISIBLE);
+            holder.folderIcon.setVisibility(View.GONE);
+            if (selectedFile.getName().toLowerCase().endsWith(".pdf")) {
+                holder.fileIcon_img.setImageResource(R.drawable.ic_icon_pdf_file);
+            } else if (selectedFile.getName().toLowerCase().endsWith(".jpg")
+                    || selectedFile.getName().toLowerCase().endsWith(".jpeg")
+                    || selectedFile.getName().toLowerCase().endsWith(".png")) {
+                holder.fileIcon_img.setImageResource(R.drawable.ic_twotone_photo_24);
+            } else if (selectedFile.getName().toLowerCase().endsWith(".xml")) {
+                holder.fileIcon_img.setImageResource(R.drawable.ic_icon_xml_file);
+            } else if (selectedFile.getName().toLowerCase().endsWith(".js")) {
+                holder.fileIcon_img.setImageResource(R.drawable.ic_icon_js_file);
+            } else if (selectedFile.getName().toLowerCase().endsWith(".xlsx")
+                    || selectedFile.getName().toLowerCase().endsWith(".csv")) {
+                holder.fileIcon_img.setImageResource(R.drawable.ic_icon_xlsx_file);
+            } else if (selectedFile.getName().toLowerCase().endsWith(".docx")) {
+                holder.fileIcon_img.setImageResource(R.drawable.ic_home_icon);
+            } else if (selectedFile.getName().toLowerCase().endsWith(".ppt")) {
+                holder.fileIcon_img.setImageResource(R.drawable.ic_icon_ppt_file);
+            } else if (selectedFile.getName().toLowerCase().endsWith(".c")
+                    || selectedFile.getName().toLowerCase().endsWith(".cpp")
+                    || selectedFile.getName().toLowerCase().endsWith(".h")
+                    || selectedFile.getName().toLowerCase().endsWith(".c++")
+                    || selectedFile.getName().toLowerCase().endsWith(".cxx")) {
+                holder.fileIcon_img.setImageResource(R.drawable.ic_icon_cpp_file);
+            } else if (selectedFile.getName().toLowerCase().endsWith(".html")) {
+                holder.fileIcon_img.setImageResource(R.drawable.ic_icon_html_2_file);
+            } else if (selectedFile.getName().toLowerCase().endsWith(".css")) {
+                holder.fileIcon_img.setImageResource(R.drawable.ic_icon_css_file);
+            } else if (selectedFile.getName().toLowerCase().endsWith(".rar")
+                    || selectedFile.getName().toLowerCase().endsWith(".zip")) {
+                holder.fileIcon_img.setImageResource(R.drawable.ic_icon_zip_folder);
+            } else if (selectedFile.getName().toLowerCase().endsWith(".apk")) {
+                holder.fileIcon_img.setImageResource(R.drawable.ic_baseline_android_24);
+            } else {
+                holder.fileIcon_img.setImageResource(R.drawable.ic_file_icon);
+            }
+
+        }
+        holder.vertDot.setOnClickListener(view -> {
+            Context context1 = new ContextThemeWrapper(context, R.style.CustomPopupMenuStyle);
+            PopupMenu popupMenu = new PopupMenu(context1, holder.vertDot);
+            popupMenu.inflate(R.menu.file_folder_options_menu);
+            if (selectedFile.isDirectory()){
+                popupMenu.getMenu().findItem(R.id.menu_file_open_with).setVisible(false);
+                popupMenu.getMenu().findItem(R.id.menu_file_share).setVisible(false);
+                popupMenu.getMenu().findItem(R.id.menu_file_addTo_favourite).setVisible(false);
+                popupMenu.getMenu().findItem(R.id.menu_file_moveTo_trash).setVisible(false);
+                popupMenu.getMenu().findItem(R.id.menu_file_moveTo_safeFolder).setVisible(false);
+                popupMenu.getMenu().findItem(R.id.menu_file_file_info).setTitle("Folder info");
+            }
+            popupMenu.setOnMenuItemClickListener(item -> {
+                switch (item.getItemId()) {
+                    case R.id.menu_file_selected:
+                        Toast.makeText(context, "clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.menu_file_copy_to:
+                        Toast.makeText(context, "copy", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.menu_file_file_info:
+                        Toast.makeText(context, "file info", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(context, Detailed_view.class);
+                        DataHolder.getInstance().setData(selectedFile.getAbsolutePath());
+                        context.startActivity(intent);
+                        break;
+                    case R.id.menu_file_delete_permanently:
+                        Toast.makeText(context, "delete", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            });
+            popupMenu.show();
+        });
     }
 
     @Override
@@ -44,12 +140,19 @@ public class Common_adapter extends RecyclerView.Adapter<Common_adapter.Common_a
 
     public class Common_adapter_ViewHolder extends RecyclerView.ViewHolder {
         TextView timeStamp, fileName;
-        ImageView vertDot;
+        ImageView vertDot, fileIcon_img;
+        CardView fileIcon;
+        LinearLayout folderIcon;
+
         public Common_adapter_ViewHolder(@NonNull View itemView) {
             super(itemView);
             timeStamp = itemView.findViewById(R.id.timeStamp_item);
             fileName = itemView.findViewById(R.id.item_folder_name);
             vertDot = itemView.findViewById(R.id.vert_three_dot_item);
+            fileIcon = itemView.findViewById(R.id.file_folder_icon);
+            folderIcon = itemView.findViewById(R.id.folder_icon_item_view);
+            fileIcon_img = itemView.findViewById(R.id.file_icon_view_item);
+
         }
     }
 }
--- app/src/main/java/com/example/fileexplorer/search_view/Detailed_view.java
+++ app/src/main/java/com/example/fileexplorer/search_view/Detailed_view.java
@@ -1,14 +1,13 @@
 package com.example.fileexplorer.search_view;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
@@ -17,17 +16,22 @@ import androidx.appcompat.widget.Toolbar;
 
 import com.example.fileexplorer.R;
 import com.example.fileexplorer.databinding.ActivityDetailedViewBinding;
+import com.example.fileexplorer.util.DataHolder;
 
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.DecimalFormat;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Detailed_view extends AppCompatActivity {
 
+    private static final String TAG = "myApp400";
     ActivityDetailedViewBinding binding;
     public static String path1;
     GetFileAndFolderSize getFileAndFolderSize = new GetFileAndFolderSize();
@@ -43,24 +47,80 @@ public class Detailed_view extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.detailed_page_titleBar);
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("");
 
 
-        Intent intent = getIntent();
-        if (intent.hasExtra("pic_folder")) {
-
-        } else if (intent.hasExtra("download_path")) {
-            path1 = intent.getStringExtra("download_path");
-        }
+        path1 = DataHolder.getInstance().getData();
+        Log.d(TAG, "onCreate: " + path1);
         Path dir = Paths.get(path1);
+        Date lastModified = new Date(dir.toFile().lastModified());
+        @SuppressLint("SimpleDateFormat")
+        Format f = new SimpleDateFormat("dd MMMM yy");
+        @SuppressLint("SimpleDateFormat")
+        Format f1 = new SimpleDateFormat("HH:mm aa");
+        String date = f.format(lastModified);
+        String timeStamp = f1.format(lastModified);
+        binding.detailedPageFolderPath.setText(path1);
+        binding.folderNameTextDetailed.setText(dir.toFile().getName());
+        binding.detailedPageModifiedDetail.setText("Modified " + date + ", " + timeStamp);
         if (dir.toFile().isDirectory()) {
             ExecuteTask();
             binding.detailedPageFolderInfo.setText("0  folders");
             binding.detailedPageFileInfo.setText("0  files");
             binding.detailedPageSizeOfFiles.setText("0  B");
         } else {
+            if (dir.toFile().toString().toLowerCase().endsWith(".pdf")) {
+                binding.detailedViewFileIcon.setImageResource(R.drawable.ic_icon_pdf_file);
+                binding.detailedPageImgFolderOrFile.setImageResource(R.drawable.ic_icon_pdf_file);
+            } else if (dir.toFile().toString().toLowerCase().endsWith(".jpg")
+                    || dir.toFile().toString().toLowerCase().endsWith(".jpeg")
+                    || dir.toFile().toString().toLowerCase().endsWith(".png")) {
+                binding.detailedViewFileIcon.setImageResource(R.drawable.ic_twotone_photo_24);
+                binding.detailedPageImgFolderOrFile.setImageResource(R.drawable.ic_twotone_photo_24);
+            } else if (dir.toFile().toString().toLowerCase().endsWith(".xml")) {
+                binding.detailedViewFileIcon.setImageResource(R.drawable.ic_icon_xml_file);
+                binding.detailedPageImgFolderOrFile.setImageResource(R.drawable.ic_icon_xml_file);
+            } else if (dir.toFile().toString().toLowerCase().endsWith(".js")) {
+                binding.detailedViewFileIcon.setImageResource(R.drawable.ic_icon_js_file);
+                binding.detailedPageImgFolderOrFile.setImageResource(R.drawable.ic_icon_js_file);
+            } else if (dir.toFile().toString().toLowerCase().endsWith(".xlsx")
+                    || dir.toFile().toString().toLowerCase().endsWith(".csv")) {
+                binding.detailedViewFileIcon.setImageResource(R.drawable.ic_icon_xlsx_file);
+                binding.detailedPageImgFolderOrFile.setImageResource(R.drawable.ic_icon_xlsx_file);
+            } else if (dir.toFile().toString().toLowerCase().endsWith(".docx")) {
+                binding.detailedViewFileIcon.setImageResource(R.drawable.ic_home_icon);
+                binding.detailedPageImgFolderOrFile.setImageResource(R.drawable.ic_home_icon);
+            } else if (dir.toFile().toString().toLowerCase().endsWith(".ppt")) {
+                binding.detailedViewFileIcon.setImageResource(R.drawable.ic_icon_ppt_file);
+                binding.detailedPageImgFolderOrFile.setImageResource(R.drawable.ic_icon_ppt_file);
+            } else if (dir.toFile().toString().toLowerCase().endsWith(".c")
+                    || dir.toFile().toString().toLowerCase().endsWith(".cpp")
+                    || dir.toFile().toString().toLowerCase().endsWith(".h")
+                    || dir.toFile().toString().toLowerCase().endsWith(".c++")
+                    || dir.toFile().toString().toLowerCase().endsWith(".cxx")) {
+                binding.detailedViewFileIcon.setImageResource(R.drawable.ic_icon_cpp_file);
+                binding.detailedPageImgFolderOrFile.setImageResource(R.drawable.ic_icon_cpp_file);
+            } else if (dir.toFile().toString().toLowerCase().endsWith(".html")) {
+                binding.detailedViewFileIcon.setImageResource(R.drawable.ic_icon_html_2_file);
+                binding.detailedPageImgFolderOrFile.setImageResource(R.drawable.ic_icon_html_2_file);
+            } else if (dir.toFile().toString().toLowerCase().endsWith(".css")) {
+                binding.detailedViewFileIcon.setImageResource(R.drawable.ic_icon_css_file);
+                binding.detailedPageImgFolderOrFile.setImageResource(R.drawable.ic_icon_css_file);
+            } else if (dir.toFile().toString().toLowerCase().endsWith(".rar")
+                    || dir.toFile().toString().toLowerCase().endsWith(".zip")) {
+                binding.detailedViewFileIcon.setImageResource(R.drawable.ic_icon_zip_folder);
+                binding.detailedPageImgFolderOrFile.setImageResource(R.drawable.ic_icon_zip_folder);
+            } else if (dir.toFile().toString().toLowerCase().endsWith(".apk")) {
+                binding.detailedViewFileIcon.setImageResource(R.drawable.ic_baseline_android_24);
+                binding.detailedPageImgFolderOrFile.setImageResource(R.drawable.ic_baseline_android_24);
+            } else {
+                binding.detailedViewFileIcon.setImageResource(R.drawable.ic_file_icon);
+                binding.detailedPageImgFolderOrFile.setImageResource(R.drawable.ic_file_icon);
+            }
             ExecuteFileInfo();
-            binding.detailedPageFileInfo.setVisibility(View.INVISIBLE);
-            binding.detailedPageSizeOfFiles.setVisibility(View.INVISIBLE);
+            binding.detailedPageFolderInfo.setVisibility(View.GONE);
+            binding.detailedPageFileInfo.setVisibility(View.GONE);
+            binding.detailedPageSizeOfFiles.setVisibility(View.VISIBLE);
         }
 
     }
